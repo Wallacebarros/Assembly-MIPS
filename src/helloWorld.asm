6a22711f6e73f362ccb50569f647acf30c07ad2a
WallaceBarros <w.wallacebarros.cruz@gmail.com>
2024-12-21 14:13:41 -0300

    exemplo: divisão por números inteiros

--- src/helloWorld.asm
+++ src/helloWorld.asm
@@ -1,7 +1,15 @@
 .text
-    li $t1, 10
-    sll $t0, $t1, 5 # t1*2^x
+
+    # Divisão de números inteiros
+
+    li $t1, 15
+    li $t2, 5
+    
+    div $t1, $t2 # dividir 15 por 5; // 3
+    
+    mflo $s0 # parte inteira da divisão
+    mfhi $s1 # resto da divisão 
 
     li $v0, 1
-    move $a0, $t0
+    move $a0, $s0
     syscall
