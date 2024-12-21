a3bcf956b63d08548b7346a80e9bb4f360bd59a5
WallaceBarros <w.wallacebarros.cruz@gmail.com>
2024-12-21 11:51:40 -0300

    soma de numeros inteiros

--- src/helloWorld.asm
+++ src/helloWorld.asm
@@ -1,9 +1,5 @@
-.data
-    numero: .word 56 # valor inteiro na memoria principal
 .text
-    li $v0, 1
-    lw $a0, numero
-    syscall
-
-    li $v0, 10
-    syscall
+    li $t1, 30 # atribuido 30 a t1
+    li $t2, 20 # atribuindo 20 a t2
+    add $t0, $t1, $t2 # somando t2 com t1 e atribuindo a t0
+    addi $t0, $t1, 20 # somando t1 com 20 e atribuindo a t0
