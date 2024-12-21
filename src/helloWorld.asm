a860e1b3704cce919d8c3bb69f35d7d904c65ff8
WallaceBarros <w.wallacebarros.cruz@gmail.com>
2024-12-21 11:55:19 -0300

    exemplo: atribuindo inteiro usando addi

--- src/helloWorld.asm
+++ src/helloWorld.asm
@@ -1,5 +1,2 @@
 .text
-    li $t1, 30 # atribuido 30 a t1
-    li $t2, 20 # atribuindo 20 a t2
-    add $t0, $t1, $t2 # somando t2 com t1 e atribuindo a t0
-    addi $t0, $t1, 20 # somando t1 com 20 e atribuindo a t0
+    addi $t1, $zero, 20 # atribuindo 20 a t1
