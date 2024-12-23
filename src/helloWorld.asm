d1c74db2fcda9583e2895aae13016e2c8dd87bbc
WallaceBarros <w.wallacebarros.cruz@gmail.com>
2024-12-23 20:20:54 -0300

    exemplo: lendo strings

--- src/helloWorld.asm
+++ src/helloWorld.asm
@@ -1,20 +1,22 @@
 .data
-    qualIdade: .asciiz "Qual a sua idade ? "
-    suaIdade: .asciiz "Sua idade e: "
+    qualNome: .asciiz "Qual seu nome ? "
+    seuNome: .asciiz "Seu nome e: "
+
+    nome: .space 25
 .text
     li $v0, 4
-    la $a0, qualIdade
+    la $a0, qualNome
     syscall
 
-    li $v0, 5
+    li $v0, 8
+    la $a0, nome
+    la $a1, 25
     syscall
-
-    move $t0, $v0
 
     li $v0, 4
-    la $a0, suaIdade
+    la $a0, seuNome
     syscall
 
-    li $v0, 1
-    move $a0, $t0
+    li $v0, 4
+    la $a0, nome
     syscall
