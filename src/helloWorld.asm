050833c7980958039821914fc5ab250d70490462
WallaceBarros <w.wallacebarros.cruz@gmail.com>
2024-12-24 08:38:51 -0300

    exemplo: estruturas condicionais

--- src/helloWorld.asm
+++ src/helloWorld.asm
@@ -1,16 +1,32 @@
 .data
-    menssage: .asciiz "Digite um numero double: "
-    zero: .double 0.0
+    digite: .asciiz "Digite um numero: "
+    impar: .asciiz "O numero e impar"
+    par: .asciiz "O numero e par"
 .text
     li $v0, 4
-    la $a0, menssage
+    la $a0, digite
     syscall
 
-    li $v0, 7 # armaena em $f0
+    li $v0, 5
     syscall
 
-    ldc1 $f2, zero
-    add.d $f12, $f2, $f0
- 
-    li $v0, 3
+    li $t0, 2
+    div $v0, $t0
+
+    mfhi $t1
+    
+    move $t0, $zero
+    bne $t1, $t0, numeroImpar
+    # beq-=, bne-!=, blt-<, bgt->, ble-<=, bge->=
+
+    li $v0, 4
+    la $a0, par
     syscall
+
+    li $v0, 10
+    syscall
+
+    numeroImpar:
+        li $v0, 4
+        la $a0, impar
+        syscall
